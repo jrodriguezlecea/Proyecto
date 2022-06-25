2963ff4e57ac755899b9daeae17730e9fa9746b7
unknown <jrodriguezlecea@gmail.com>
2022-06-25 15:55:18 -0300

    Primer Cambio

--- Proyecto/settings.py
+++ Proyecto/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Proyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [r'C:\Users\julian\Desktop\Coder\Trabajo\templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
